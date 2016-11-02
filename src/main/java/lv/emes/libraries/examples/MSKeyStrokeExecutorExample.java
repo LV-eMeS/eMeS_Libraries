6cccef33433d8199e219575515fe318f7f6c04e7
DIR\maris.salenieks <maris.salenieks@accenture.com>
2016-11-02 15:37:13 +0200

    Removed lib folder. Those dependencies now are linked with Gradle too.

--- src/main/java/lv/emes/libraries/examples/MSKeyStrokeExecutorExample.java
+++ src/main/java/lv/emes/libraries/examples/MSKeyStrokeExecutorExample.java
@@ -4,6 +4,8 @@ import lv.emes.libraries.tools.platform.MS_KeyStrokeExecutor;
 
 import java.awt.*;
 
+import static junit.framework.TestCase.assertTrue;
+
 /** 
  * @version 1.0.
  * @author eMeS
@@ -12,13 +14,14 @@ public class MSKeyStrokeExecutorExample {
 
 	public static void main(String[] args) {
 		System.out.println("Execution begins.");
-		Robot rob = null;
 		MS_KeyStrokeExecutor.getInstance().mouseSetCoords(new Point(111, 111));
 		MS_KeyStrokeExecutor.getInstance().mouseMove(new Point(50, 50));
-//		rob.mouseMove(111, 111);
-//		MS_KeyStrokeExecutor.getInstance().keyPress("A");
-//		rob.keyPress(65);
-//		rob.keyRelease(65);
+		MS_KeyStrokeExecutor.getInstance().keyPress("A");
+		MS_KeyStrokeExecutor.getInstance().keyPress("a");
+
+		MS_KeyStrokeExecutor.getInstance().keyDown("up");
+		System.out.println(MS_KeyStrokeExecutor.getInstance().isKeyDown("up"));
+		MS_KeyStrokeExecutor.getInstance().keyUp("up");
 		System.out.println("Execution ends.");
 	}
 }
