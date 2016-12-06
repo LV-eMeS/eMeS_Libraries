4ae8bb9a281abaa60f0dc786be3a0bea0b289d85
maris-salenieks <zmaster5@inbox.lv>
2016-12-06 21:11:35 +0200

    Created small HTTP client for TODO.

---
+++ src/main/java/lv/emes/libraries/communication/http/client/MS_HttpClient.java
@@ -0,0 +1,28 @@
+package lv.emes.libraries.communication.http.client;
+
+/**
+ * Description.
+ * <p>Public methods:
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <p>Static methods:
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <p>Properties:
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <p>Setters and getters:
+ * <ul>
+ * <li></li>
+ * </ul>
+ *
+ * @author eMeS
+ * @version 1.0.
+ */
+public class MS_HttpClient {
+    //TODO http://blog.modulus.io/build-your-first-http-server-in-nodejs
+    //TODO http://stackoverflow.com/questions/2938502/sending-post-data-in-android
+}
