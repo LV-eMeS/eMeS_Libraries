43f1188de7a27b69474c4a3173bab0478c04f434
maris-salenieks <zmaster5@inbox.lv>
2016-12-28 15:02:23 +0200

    MS_TakenPorts created as global constants for every eMeS project.

---
+++ src/main/java/lv/emes/libraries/communication/MS_TakenPorts.java
@@ -0,0 +1,18 @@
+package lv.emes.libraries.communication;
+
+/**
+ * This class consists of constants of already taken ports by eMeS Software.
+ *
+ * @author eMeS
+ * @version 1.0.
+ */
+public class MS_TakenPorts {
+    public static final int DEFAULT_PORT_FOR_TESTING = 12343;
+    public static final int NUMBERATEGY_SERVER = 12344;
+    public static final int NUMBERATEGY_CLIENT = 12345;
+    public static final int FTP_COMMANDER_DEAD = 12346;
+    public static final int JAVA_PROGRAM_LAUNCHER = 12347;
+    public static final int SPRING_WEB_SERVER = 12348;
+
+    public static final int LAST_PORT_NUMBER = 12400; //last port of eMeS app usages.
+}
