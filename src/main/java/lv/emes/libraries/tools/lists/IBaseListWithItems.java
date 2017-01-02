8950e7f32987dc353a2120fc0cbf9e80e47b5fe0
maris-salenieks <zmaster5@inbox.lv>
2017-01-02 15:41:06 +0200

    Updated documentation.

--- src/main/java/lv/emes/libraries/tools/lists/IBaseListWithItems.java
+++ src/main/java/lv/emes/libraries/tools/lists/IBaseListWithItems.java
@@ -1,95 +1,118 @@
 package lv.emes.libraries.tools.lists;
+
 /**
  * Interface defines method for lists every item to perform.
  * Method:<br>
- *     -doWithEveryItem;
+ * -doWithEveryItem;
+ *
  * @param <T> type of list elements.
- * @version 1.1.
+ * @version 1.2.
  */
 public interface IBaseListWithItems<T> {
-	/**
-	 * @return count of elements in list. [0..+]
-	 */
-	int count();
+    /**
+     * @return count of elements in list. [0..+]
+     */
+    int count();
 
-	/**
-	 * Just an overloaded method of <b>count()</b>.
-	 * @return count of items in list.
-	 * @see IBaseListWithItems#count()
-	 */
-	default int size() {
-		return count();
-	}
+    /**
+     * Just an overloaded method of <b>count()</b>.
+     *
+     * @return count of items in list.
+     * @see IBaseListWithItems#count()
+     */
+    default int size() {
+        return count();
+    }
 
-	/**
-	 * Just an overloaded method of <b>count()</b>.
-	 * @return count of items in list.
-	 * @see IBaseListWithItems#count()
-	 */
-	default int length() {
-		return count();
-	}
+    /**
+     * Just an overloaded method of <b>count()</b>.
+     *
+     * @return count of items in list.
+     * @see IBaseListWithItems#count()
+     */
+    default int length() {
+        return count();
+    }
 
-	//methods of perambulation
-	//------------------------------------------------------------------------------------------------------------------------
-	/**
-	 * Gets element by index.
-	 * @param aIndex index of element in the list.
-	 * @return element of type T.
-	 */
-	T get(int aIndex);
+    //methods of perambulation
+    //------------------------------------------------------------------------------------------------------------------------
 
-	/**
-	 * Does break for method <b>doWithEveryItem</b>.
-	 * In other words it is calling <b>setBreakDoWithEveryItem(false)</b>.
-	 */
-	default void breakDoWithEveryItem(){
-		setBreakDoWithEveryItem(true);
-	}
+    /**
+     * Gets element by index.
+     *
+     * @param aIndex index of element in the list.
+     * @return element of type T.
+     */
+    T get(int aIndex);
 
-	/**
-	 * Implement setting of flag's for loop breaking value when inside of <b>doWithEveryItem</b> method!
-	 * @param value if true then loop will be broken and next iteration will not be reached.
-	 */
-	void setBreakDoWithEveryItem(boolean value);
+    /**
+     * Does break for method <b>doWithEveryItem</b>.
+     * In other words it is calling <b>setBreakDoWithEveryItem(false)</b>.
+     */
+    default void breakDoWithEveryItem() {
+        setBreakDoWithEveryItem(true);
+    }
 
-	/**
-	 * Flag for <b>doWithEveryItem</b> method that loop must be broken and execution of code must continue
-	 * to flow for next commands after loop.
-	 * @return if true loop will be broken and next iteration will not be executed;
-	 * if false, loop will continue to next list item.
-	 */
-	boolean getBreakDoWithEveryItem();
+    /**
+     * Implement setting of flag's for loop breaking value when inside of <b>doWithEveryItem</b> method!
+     *
+     * @param value if true then loop will be broken and next iteration will not be reached.
+     */
+    void setBreakDoWithEveryItem(boolean value);
 
-	/**
-	 * Walks through all the elements in list and does the defined action <b>action</b>.
-	 * <code>
-     * MS_StringList sl = new MS_StringList("One#Two#");
-     * sl.doWithEveryItem((s, index) -&gt; {
-	 * 	System.out.println(s);
-	 * 	System.out.println(index);
-     * });
-	 * </code>
-	 * Loop is broken if getBreakDoWithEveryItem() returns true.
-	 * For example, code below will be executed only with first element even if there is more elements in list.
-	 * <code>
-	 * MS_StringList sl = new MS_StringList("One#Two#");
-	 * sl.doWithEveryItem((s, index) -&gt; {
-	 * 	System.out.println(s);
-	 * 	setBreakDoWithEveryItem(true);
-	 * });
-	 * </code>
-	 * @param action method that describes the actions that every element must do.
-	 *               Method's incoming parameters are every item of the list and index of item.
-	 */
-	default void doWithEveryItem(IFuncSomeAction<T> action) {
-		setBreakDoWithEveryItem(false);
-		if (action != null)
-			for (int i = 0; i < this.count(); i++) {
-				T itm = this.get(i);
-				action.doAction(itm, i);
-				if (getBreakDoWithEveryItem())
-					break;
-			}
-	}
+    /**
+     * Flag for <b>doWithEveryItem</b> method that loop must be broken and execution of code must continue
+     * to flow for next commands after loop.
+     *
+     * @return if true loop will be broken and next iteration will not be executed;
+     * if false, loop will continue to next list item.
+     */
+    boolean getBreakDoWithEveryItem();
+
+    /**
+     * Walks through all the elements in list and does the defined action <b>action</b>.<br>
+     * <code>
+     * MS_StringList sl = new MS_StringList("One#Two#");<br>
+     * sl.doWithEveryItem((s, index) -&gt; {<br>
+     * System.out.println(s);<br>
+     * System.out.println(index);<br>
+     * });<br>
+     * </code>
+     * <p>
+     * Loop is broken if getBreakDoWithEveryItem() returns true.
+     * For example, code below will be executed only with first element even if there is more elements in list.<br>
+     * <code>
+     * MS_StringList sl = new MS_StringList("One#Two#");<br>
+     * sl.doWithEveryItem((s, index) -&gt; {<br>
+     * System.out.println(s);<br>
+     * setBreakDoWithEveryItem(true);<br>
+     * });<br>
+     * </code>
+     * <p>
+     * <p>
+     * If some temporary variable is needed for lambda expression, just use <b>AtomicReference</b>!.<br>
+     * <code>
+     * MS_StringList sl = new MS_StringList("One#Two#");<br>
+     * final AtomicReference<Boolean> itemFound = new AtomicReference<>();<br>
+     * sl.doWithEveryItem((s, index) -&gt; {<br>
+     * System.out.println(s);<br>
+     * if (s.equals("Two"))<br>
+     * itemFound.set(true);<br>
+     * setBreakDoWithEveryItem(itemFound.get());<br>
+     * });<br>
+     * </code>
+     *
+     * @param action method that describes the actions that every element must do.
+     *               Method's incoming parameters are every item of the list and index of item.
+     */
+    default void doWithEveryItem(IFuncSomeAction<T> action) {
+        setBreakDoWithEveryItem(false);
+        if (action != null)
+            for (int i = 0; i < this.count(); i++) {
+                T itm = this.get(i);
+                action.doAction(itm, i);
+                if (getBreakDoWithEveryItem())
+                    break;
+            }
+    }
 }
