7ef70b3c3bcd0ae673cf74fe3bacb30ed0a8734f
maris-salenieks <zmaster5@inbox.lv>
2017-02-22 22:53:51 +0200

    Hostname changed to http://emeslv.sytes.net.

--- src/test/java/lv/emes/libraries/communication/CommunicationConstants.java
+++ src/test/java/lv/emes/libraries/communication/CommunicationConstants.java
@@ -6,6 +6,6 @@ package lv.emes.libraries.communication;
  * @version 1.0.
  */
 public class CommunicationConstants {
-    public static final String TESTING_SERVER_HOSTAME = "emesserver.ddns.net";
+    public static final String TESTING_SERVER_HOSTAME = "emeslv.sytes.net";
     public static final String TESTING_SERVER_PORT_STRING = ":12348";
 }
