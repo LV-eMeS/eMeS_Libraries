b921de9ca8f46518adce86c71f2127a41447c65b
maris-salenieks <zmaster5@inbox.lv>
2017-05-30 23:16:45 +0300

    Created MS_Scheduler class and left TODO's to implement it in future

---
+++ src/main/java/lv/emes/libraries/utilities/MS_Scheduler.java
@@ -0,0 +1,31 @@
+package lv.emes.libraries.utilities;
+
+/**
+ * An mechanism that allows to do actions in some period of time.
+ * Those actions can be either periodic either meant for running once.
+ * Precision of this scheduler is designed to be one second, which means that
+ * <p>Public methods:
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <p>Static methods:
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <p>Properties:
+ * <ul>
+ * <li></li>
+ * </ul>
+ * <p>Setters and getters:
+ * <ul>
+ * <li></li>
+ * </ul>
+ *
+ * @author eMeS
+ * @version 1.0.
+ */
+public class MS_Scheduler {
+    //TODO implement this using slep methods and stuff
+    //TOD rethink, if we need 1 class for both actions - scheduling time and do periodic actions in specific time interval
+    //or just to create 2 class with 2 different behavior
+}
