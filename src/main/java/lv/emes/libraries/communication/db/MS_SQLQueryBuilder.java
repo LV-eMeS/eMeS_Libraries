08ee83a223fe4126604f2fe27d354132bc2393b1
maris-salenieks <zmaster5@inbox.lv>
2018-03-03 16:18:34 +0200

    Make "insert/replace into" constructions more intuitive to write using MS_SQLQueryBuilder

--- src/main/java/lv/emes/libraries/communication/db/MS_SQLQueryBuilder.java
+++ src/main/java/lv/emes/libraries/communication/db/MS_SQLQueryBuilder.java
@@ -11,11 +11,9 @@ import lv.emes.libraries.utilities.MS_StringUtils;
  * or {@link MS_SQLQueryBuilder#append(String)} methods.
  *
  * @author eMeS
- * @version 2.1.
+ * @version 2.2.
  */
 public class MS_SQLQueryBuilder extends MS_LineBuilder {
-
-    //TODO document this whole class methods
 
     public static final String _QPARAM = "?";
     public static final String _SP = " "; //space
@@ -53,60 +51,203 @@ public class MS_SQLQueryBuilder extends MS_LineBuilder {
     private Oper previousOperation = null;
     private byte fieldOperationCalledTimes = 0;
 
+    /**
+     * Appends query with "SELECT".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder select() {
         return beginOperation(true).appendInternal(Oper.SELECT.toString()).endOperation(Oper.SELECT);
     }
 
+    /**
+     * Appends query with "INSERT INTO".
+     *
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#insertInto(String)
+     * @see MS_SQLQueryBuilder#insertInto(String, MS_StringList)
+     */
     public MS_SQLQueryBuilder insertInto() {
         return beginOperation(true).appendInternal(Oper.INSERT_INTO.toString()).endOperation(Oper.INSERT_INTO);
     }
 
+    /**
+     * Appends query with "INSERT INTO <b>tableName</b>".
+     *
+     * @param tableName table name to append to query.
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#insertInto()
+     * @see MS_SQLQueryBuilder#insertInto(String, MS_StringList)
+     */
     public MS_SQLQueryBuilder insertInto(String tableName) {
         return insertInto().table(tableName);
     }
 
+    /**
+     * Appends query with "INSERT INTO <b>tableName</b>(<b>fieldNames.1</b>, <b>fieldNames.2</b>...)".
+     * Parameter <b>fieldNames</b> should be a string list containing 1 or more field names.
+     * No null checks or list length checks are performed here, so make sure list is correct before calling this method!
+     * <p><u>Example of use</u>:
+     * <code>sqlBuilder.insertInto("products", new MS_StringList("id,name,price", ','))</code>
+     *
+     * @param tableName  table name to append to query.
+     * @param fieldNames eMeS string list of field names.
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#insertInto()
+     * @see MS_SQLQueryBuilder#insertInto(String)
+     */
     public MS_SQLQueryBuilder insertInto(String tableName, MS_StringList fieldNames) {
         insertInto(tableName).bracketOpening();
         fieldNames.forEachItem((field, i) -> field(field));
         return bracketClosing();
     }
 
+    /**
+     * Appends query with "REPLACE INTO".
+     *
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#replaceInto(String)
+     * @see MS_SQLQueryBuilder#replaceInto(String, MS_StringList)
+     */
     public MS_SQLQueryBuilder replaceInto() {
         return beginOperation(true).appendInternal(Oper.REPLACE_INTO.toString()).endOperation(Oper.REPLACE_INTO);
     }
 
+    /**
+     * Appends query with "REPLACE INTO <b>tableName</b>".
+     *
+     * @param tableName table name to append to query.
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#replaceInto()
+     * @see MS_SQLQueryBuilder#replaceInto(String, MS_StringList)
+     */
     public MS_SQLQueryBuilder replaceInto(String tableName) {
         return replaceInto().table(tableName);
     }
 
+    /**
+     * Appends query with "REPLACE INTO <b>tableName</b>(<b>fieldNames.1</b>, <b>fieldNames.2</b>...)".
+     * Parameter <b>fieldNames</b> should be a string list containing 1 or more field names.
+     * No null checks or list length checks are performed here, so make sure list is correct before calling this method!
+     * <p><u>Example of use</u>:
+     * <code>sqlBuilder.replaceInto("products", new MS_StringList("id,name,price", ','))</code>
+     *
+     * @param tableName  table name to append to query.
+     * @param fieldNames eMeS string list of field names.
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#replaceInto()
+     * @see MS_SQLQueryBuilder#replaceInto(String)
+     */
+    public MS_SQLQueryBuilder replaceInto(String tableName, MS_StringList fieldNames) {
+        replaceInto(tableName).bracketOpening();
+        fieldNames.forEachItem((field, i) -> field(field));
+        return bracketClosing();
+    }
+
+    /**
+     * Appends query with "UPDATE".
+     *
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#update(String)
+     */
     public MS_SQLQueryBuilder update() {
         return beginOperation(true).appendInternal(Oper.UPDATE.toString()).endOperation(Oper.UPDATE);
     }
 
+    /**
+     * Appends query with "UPDATE <b>tableName</b>".
+     *
+     * @param tableName table name to append to query.
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#update()
+     */
     public MS_SQLQueryBuilder update(String tableName) {
         return update().table(tableName).set();
     }
 
+    /**
+     * Appends query with "DELETE FROM".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder deleteFrom() {
         return beginOperation(true).appendInternal(Oper.DELETE_FROM.toString()).endOperation(Oper.DELETE_FROM);
     }
 
     //other operations
 
+    /**
+     * Appends query with "*".
+     * Should be used in SELECT constructions after calling {@link MS_SQLQueryBuilder#select()}.
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder all() {
         return beginOperation(false).appendInternal(_ALL).endOperation(Oper.FIELD);
     }
 
+    /**
+     * Appends query with field name <b>fieldName</b> and if previous call was <b>field(String fieldName)</b> as well,
+     * appends query with comma ", " before this <b>fieldName</b>.
+     * Also null check is performed and, in case <b>fieldName</b> = <code>null</code>, query is appended with "null" instead.
+     *
+     * @param fieldName field name to append to query.
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder field(String fieldName) {
         if (fieldName == null) fieldName = "null";
         if (Oper.FIELD.equals(previousOperation)) this.appendInternal(_COMMA);
         return beginOperation(false).appendInternal(fieldName).endOperation(Oper.FIELD);
     }
 
+    /**
+     * Appends query with "VALUES".
+     *
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#values(MS_StringList)
+     */
+    public MS_SQLQueryBuilder values() {
+        return beginOperation(true).appendInternal(Oper.VALUES.toString()).endOperation(Oper.VALUES);
+    }
+
+    /**
+     * Appends query with "VALUES (<b>valueList.1</b>, <b>valueList.2</b>...)".
+     * Parameter <b>valueList</b> should be a string list containing 1 or more field values.
+     * No null checks or list length checks are performed here, so make sure list is correct before calling this method!
+     * <p><u>Example of use</u>:
+     * <code>sqlBuilder.values(new MS_StringList("null,orange,0.75", ','))</code>
+     *
+     * @param valueList eMeS string list of field values.
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#values()
+     */
+    public MS_SQLQueryBuilder values(MS_StringList valueList) {
+        this.values().bracketOpening();
+        valueList.forEachItem((fieldValue, i) -> value(fieldValue));
+        return bracketClosing();
+    }
+
+    /**
+     * Appends query with field value <b>value</b>.
+     * Should be used in INSERT INTO or REPLACE INTO constructions after calling {@link MS_SQLQueryBuilder#values()}
+     * and {@link MS_SQLQueryBuilder#bracketOpening()}.
+     *
+     * @param value value of field to be inserted or replaced.
+     * @return reference to this query itself.
+     * @see MS_SQLQueryBuilder#field(String)
+     */
     public MS_SQLQueryBuilder value(String value) {
         return field(value);
     }
 
+    /**
+     * Appends query with field name <b>fieldName</b> and value <b>fieldValue</b> like "<b>fieldName</b> = <b>fieldValue</b>".
+     * Should be used in UPDATE ... SET constructions after calling {@link MS_SQLQueryBuilder#set()}.
+     *
+     * @param fieldName  name of field which value will be updated.
+     * @param fieldValue new value for field to be updated.
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder value(String fieldName, String fieldValue) {
         if (fieldName == null) fieldName = "null";
         if (fieldValue == null) fieldValue = "null";
@@ -116,70 +257,134 @@ public class MS_SQLQueryBuilder extends MS_LineBuilder {
                 .endOperation(Oper.VALUE);
     }
 
-    public MS_SQLQueryBuilder values() {
-        return beginOperation(true).appendInternal(Oper.VALUES.toString()).endOperation(Oper.VALUES);
-    }
-
-    public MS_SQLQueryBuilder values(MS_StringList valueList) {
-        this.values().bracketOpening();
-        valueList.forEachItem((fieldValue, i) -> value(fieldValue));
-        return bracketClosing();
-    }
-
+    /**
+     * Appends query with table name <b>tableName</b>.
+     *
+     * @param tableName table name.
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder table(String tableName) {
         return beginOperation(false).appendInternal(tableName).endOperation(Oper.TABLE);
     }
 
+    /**
+     * Appends query with "SET".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder set() {
         return beginOperation(false).appendInternal(Oper.SET.toString()).endOperation(Oper.SET);
     }
 
+    /**
+     * Appends query with "FROM".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder from() {
         return beginOperation(Oper.FIELD.equals(previousOperation) && fieldOperationCalledTimes > 3)
                 .appendInternal(Oper.FROM.toString()).endOperation(Oper.FROM);
     }
 
+    /**
+     * Appends query with "(".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder bracketOpening() {
         return appendInternal(_LB).endOperation(Oper.BRACKET_OPENING);
     }
 
+    /**
+     * Appends query with ")".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder bracketClosing() {
         return appendInternal(_RB).endOperation(Oper.CUSTOM);
     }
 
+    /**
+     * Appends query with "WHERE".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder where() {
         return beginOperation(true).appendInternal(Oper.WHERE.toString()).endOperation(Oper.WHERE);
     }
 
+    /**
+     * Appends query with what ever condition <b>cond</b> is expected after WHERE clause.
+     *
+     * @param cond expression that should be used in WHERE expression possibly in combination with
+     *             {@link MS_SQLQueryBuilder#and()} and/or {@link MS_SQLQueryBuilder#or()}
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder condition(String cond) {
         return beginOperation(false).appendInternal(cond).endOperation(Oper.CONDITION);
     }
 
+    /**
+     * Appends query with "UNION".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder union() {
         return beginOperation(true).appendInternal(Oper.UNION.toString()).endOperation(Oper.UNION);
     }
 
+    /**
+     * Appends query with "AND".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder and() {
         return beginOperation(false).appendInternal(Oper.AND.toString()).endOperation(Oper.AND);
     }
 
+    /**
+     * Appends query with "OR".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder or() {
         return beginOperation(false).appendInternal(Oper.OR.toString()).endOperation(Oper.OR);
     }
 
+    /**
+     * Appends query with "ORDER BY".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder orderBy() {
         return beginOperation(true).appendInternal(Oper.ORDER_BY.toString()).endOperation(Oper.ORDER_BY);
     }
 
+    /**
+     * Appends query with "ORDER BY <b>conditions</b>".
+     *
+     * @param conditions usually some single or comma delimited field name or expression collection.
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder orderBy(String conditions) {
         orderBy();
         return beginOperation(false).appendInternal(conditions).endOperation(Oper.CONDITION);
     }
 
+    /**
+     * Appends query with "ASC".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder ascending() {
         return beginOperation(false).appendInternal("ASC").endOperation(Oper.CUSTOM);
     }
 
+    /**
+     * Appends query with "DESC".
+     *
+     * @return reference to this query itself.
+     */
     public MS_SQLQueryBuilder descending() {
         return beginOperation(false).appendInternal("DESC").endOperation(Oper.CUSTOM);
     }
@@ -214,6 +419,7 @@ public class MS_SQLQueryBuilder extends MS_LineBuilder {
 
     /**
      * In order to finalize and build this SQL query appends semicolon at the end of statement.
+     *
      * @return reference to this query itself.
      */
     public MS_SQLQueryBuilder build() {
@@ -222,6 +428,7 @@ public class MS_SQLQueryBuilder extends MS_LineBuilder {
 
     /**
      * Appends semicolon at the end of statement and returns prepared SQL query string.
+     *
      * @return SQL string representing all the parts set for this query in building process.
      */
     public String buildAndToString() {
